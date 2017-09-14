7b125b65bb4a04cdbb1dfb756d0b8aa0a60c184c
Szymon Korzeniowski <szymon.korzeniowski@monterail.com>
2017-09-14 17:50:37 +0200

    Add createdAt for posts/comments

--- fakeData.js
+++ fakeData.js
@@ -10,14 +10,16 @@ const users = Array.from({length: 10}, () => ({
 const posts = Array.from({length: 50}, () => ({
   id: faker.random.uuid(),
   authorId: faker.random.arrayElement(users).id,
-  content: faker.lorem.sentences(Math.random() * 3)
+  content: faker.lorem.sentences(Math.random() * 3),
+  createdAt: faker.date.recent(30)
 }));
 
 const comments = Array.from({length: 500}, () => ({
   id: faker.random.uuid(),
   postId: faker.random.arrayElement(posts).id,
   authorId: faker.random.arrayElement(users).id,
-  content: faker.lorem.sentences(Math.random() * 3)
+  content: faker.lorem.sentences(Math.random() * 3),
+  createdAt: faker.date.recent(30)
 }));
 
 exports.users = users;
